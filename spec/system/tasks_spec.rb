91b1298e5e679cef1042610192dc8008190efc9b
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2021-03-19 23:42:15 +0900

    add: rspecでのテスト(task->index,new,show)

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+	let(:user_a) {FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')}
+	let(:user_b) {FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')}
+	let!(:task_a) {FactoryBot.create(:task, name: '最初のタスク', user: user_a)}
+
+	before do
+		# ユーザーAでログインする
+		visit login_path
+		fill_in 'session_email', with: login_user.email # fill_inの値はラベルのforと同じにする
+		fill_in 'session_password', with: login_user.password
+		click_button 'login' # click_buttonの値はボタンのvalueと同じにする
+	end
+
+	shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+		it { expect(page).to have_content '最初のタスク' }
+	end
+
+	describe '一覧表示機能' do
+		context 'ユーザーAがログインしているとき' do
+			let(:login_user) { user_a }
+
+			it_behaves_like 'ユーザーAが作成したタスクが表示される'
+		end
+
+		context 'ユーザーBがログインしているとき' do
+			let(:login_user) { user_b }
+
+			it 'ユーザーAが作成したタスクが表示されない' do
+				# ユーザーAが作成したタスクの名称が画面上に表示されていないことを確認
+				expect(page).not_to have_content '最初のタスク'
+			end
+		end
+	end
+
+	describe '詳細表示機能' do
+		context 'ユーザーAがログインしているとき' do
+			let(:login_user) { user_a }
+
+			before do
+				visit task_path(task_a)
+			end
+
+			it_behaves_like 'ユーザーAが作成したタスクが表示される'
+		end
+	end
+
+	describe '新規作成機能' do
+		let(:login_user) { user_a }
+
+		before do
+			visit new_task_path
+			fill_in 'task_name', with: task_name
+			click_button 'Create Task'
+		end
+
+		context '新規作成画面で名称を入力したとき' do
+			let(:task_name) { '新規作成のテストを書く' }
+
+			it '正常に登録される' do
+				expect(page).to have_selector '#notice', text: 'Task was successfully created.'
+			end
+		end
+
+		context '新規作成画面で名称を入力しなかったとき' do
+			let(:task_name) { '' }
+
+			it 'エラーとなる' do
+				within '#error_explanation' do
+					expect(page).to have_content "Name can't be blank"
+				end
+			end
+		end
+	end
+end
